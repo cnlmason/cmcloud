48ab8160d926a40a6a5dccb897cf97324b12c820
Mason <Chuck.Mason@ecolab.com>
2018-07-26 15:09:08 -0500

    first commit

---
+++ GitConsoleApplication/GitConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitConsoleApplication
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+
+            //Keep teh console widown open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
